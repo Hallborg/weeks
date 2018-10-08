c16038086b33bdfaa59b52be7abf57972498f3fa
Jesper Hallborg <jesper.hallis@gmail.com>
2018-10-08 12:45:19 +0200

    Fix start-up in dockerized mode
    The binding mustn't be on localhost

--- src/main/scala/hallborg/weeks/Main.scala
+++ src/main/scala/hallborg/weeks/Main.scala
@@ -2,6 +2,7 @@ package hallborg.weeks
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.{ExceptionHandler, RejectionHandler}
 import akka.stream.ActorMaterializer
 import com.typesafe.scalalogging.LazyLogging
 import hallborg.weeks.routes.TopRoute
@@ -9,13 +10,16 @@ import hallborg.weeks.routes.TopRoute
 object Main extends LazyLogging {
   def main(args: Array[String]) {
 
-    implicit val system = ActorSystem("my-system")
-    implicit val materializer = ActorMaterializer()
-    implicit def rejectionHandle = Handlers.rejectionHandle
-    implicit def exceptionHandle = Handlers.exceptionHandler
+    implicit val system: ActorSystem = ActorSystem("my-system")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+    implicit def rejectionHandle: RejectionHandler = Handlers.rejectionHandle
+
+    implicit def exceptionHandle: ExceptionHandler = Handlers.exceptionHandler
+
     val route = new TopRoute().route
 
-    Http().bindAndHandle(route, "localhost", 8080)
-    logger.info("Server running on localhost:8080")
+    Http().bindAndHandle(route, "0.0.0.0", 8080)
+    logger.info("Server running on 0.0.0.0:8080")
   }
 }
